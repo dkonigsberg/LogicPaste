0104cd39990d3333fdb4a0ae131be1dc297a1741
Derek Konigsberg <dkonigsberg@logicprobe.org>
2012-11-30 17:36:41 -0800

    Added stderr logging hook

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,45 @@
 
 using namespace bb::cascades;
 
+#ifndef QT_NO_DEBUG_OUTPUT
+void logMessageHandler(QtMsgType type, const char *msg)
+{
+    QString debugdate = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
+    switch (type)
+    {
+    case QtDebugMsg:
+        debugdate += "[D]";
+        break;
+    case QtWarningMsg:
+        debugdate += "[W]";
+        break;
+    case QtCriticalMsg:
+        debugdate += "[C]";
+        break;
+    case QtFatalMsg:
+        debugdate += "[F]";
+        break;
+    }
+
+    fprintf(stderr, "%s %s\n", debugdate.toLatin1().data(), msg);
+
+    if (QtFatalMsg == type) {
+        abort();
+    }
+}
+#endif
+
 Q_DECL_EXPORT int main(int argc, char **argv)
-{    
+{
+#ifndef QT_NO_DEBUG_OUTPUT
+    qInstallMsgHandler(logMessageHandler);
+#endif
+
     Application app(argc, argv);
 
     QTranslator translator;
     QString locale_string = QLocale().name(); 
-    QString filename = QString( "LogicPaste_%1" ).arg( locale_string );
+    QString filename = QString("LogicPaste_%1").arg(locale_string);
     if (translator.load(filename, "app/native/qm")) {
         app.installTranslator( &translator );
     }
